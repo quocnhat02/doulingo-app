f4c5201e4f5d853d05c9aedc7920317768dfadd4
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-01 20:54:15 +0700

    migrations

---
+++ src/AuctionService/Data/AuctionDBContext.cs
@@ -0,0 +1,13 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDBContext : DbContext
+{
+    public AuctionDBContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+}
