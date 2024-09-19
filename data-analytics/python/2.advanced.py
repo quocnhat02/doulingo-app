aaa74cca4125dc5584b7152ffcbd3118a6e2cf1a
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-09-19 17:44:21 +0700

    numpy 3

--- data-analytics/python/2.advanced.py
+++ data-analytics/python/2.advanced.py
@@ -1,26 +1,34 @@
 import numpy as np
 
 
-# Tạo mảng một chiều
-arr = np.array([1, 2, 3, 4, 5])
-print("Array:", arr)
+# # Tạo mảng một chiều
+# arr = np.array([1, 2, 3, 4, 5])
+# print("Array:", arr)
 
-# Tạo mảng hai chiều
-arr2d = np.array([[1, 2, 3], [4, 5, 6]])
-print("2D Array:\n", arr2d)
+# # Tạo mảng hai chiều
+# arr2d = np.array([[1, 2, 3], [4, 5, 6]])
+# print("2D Array:\n", arr2d)
 
-# Mảng các số không
-zeros = np.zeros((3, 4))
-print("Zeros Array:\n", zeros)
+# # Mảng các số không
+# zeros = np.zeros((3, 4))
+# print("Zeros Array:\n", zeros)
 
-# Mảng các số một
-ones = np.ones((2, 3))
-print("Ones Array:\n", ones)
+# # Mảng các số một
+# ones = np.ones((2, 3))
+# print("Ones Array:\n", ones)
 
-# Mảng số nguyên từ 0 đến 9
-arange = np.arange(10)
-print("Arange Array:", arange)
+# # Mảng số nguyên từ 0 đến 9
+# arange = np.arange(10)
+# print("Arange Array:", arange)
 
-# Mảng số từ 0 đến 1 với bước nhảy 0.1
-linspace = np.linspace(0, 1, 5)
-print("Linspace Array:", linspace)
+# # Mảng số từ 0 đến 1 với bước nhảy 0.1
+# linspace = np.linspace(0, 1, 5)
+# print("Linspace Array:", linspace)
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+print("Addition:", arr1 + arr2)
+print("Subtraction:", arr1 - arr2)
+print("Multiplication:", arr1 * arr2)
+print("Division:", arr1 / arr2)
