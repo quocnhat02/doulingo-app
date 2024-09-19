14d3b1268216725eeb81f1c64611c51e83454ae4
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-09-19 21:26:16 +0700

    Phần 2: Kiến thức trung cấp về Pandas

--- data-analytics/python/3.advanced-pandas.py
+++ data-analytics/python/3.advanced-pandas.py
@@ -28,6 +28,33 @@ df['Salary'] = [50000, 60000, 70000, 80000]
 # df.drop(columns=['Salary'], inplace=True)
 
 # Xóa hàng:
-df.drop(index=0, inplace=True)  # Xóa hàng đầu tiên
+# df.drop(index=0, inplace=True)  # Xóa hàng đầu tiên
+
+# 5. Xử lý dữ liệu bị thiếu(Missing Data)
+# Trong phân tích dữ liệu, việc xử lý dữ liệu bị thiếu là rất quan trọng.
+# df.isnull().sum()
+
+# # Điền bằng giá trị trung bình
+# df['Age'].fillna(df['Age'].mean(), inplace=True)
+
+# df.dropna(axis=0, inplace=True)  # Xóa các hàng chứa giá trị thiếu
+
+# 6. Nhóm dữ liệu (GroupBy)
+# Pandas cung cấp phương thức groupby để nhóm dữ liệu theo một cột và thực hiện các phép tính như tổng, trung bình.
+# df.groupby('Category')['Revenue'].sum()
+
+# df.groupby('Category')['Revenue'].mean()
+
+# 7. Thao tác với chuỗi ký tự(String Handling)
+# Làm việc với dữ liệu dạng chuỗi:
+# df['Name'] = df['Name'].str.upper()  # Chuyển tất cả sang chữ hoa
+# df[df['City'].str.contains('New')]
+
+# 8. Ghép và nối DataFrames(Merging & Joining)
+# Bạn có thể ghép hai DataFrames với nhau giống như việc thực hiện các thao tác JOIN trong SQL.
+# df1.merge(df2, on='ID', how='inner')  # Inner join
+
+# pd.concat([df1, df2], axis=0)  # Nối theo hàng
+
 
 print(df)
