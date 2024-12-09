167fc7803b8b17c72a8ac0a7392ba106137384a4
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-09 17:01:20 +0700

    setup front end

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-green-500'>Hello World</h1>
+      <Button size={'lg'} variant='default'>
+        Button
+      </Button>
+    </div>
+  );
+}
