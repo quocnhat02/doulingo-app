399b35b06a1697ee20a455c137f8caed1960215c
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-07-13 17:56:16 +0700

    init project

---
+++ backend/config-server/src/main/java/com/duolingo/clone/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.duolingo.clone.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
