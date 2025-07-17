4877fe987da50c87f396d5f50505d8f6dce2d393
nhatnguyen <nguyenquocnhat12b1@gmail.com>
2025-07-17 23:04:01 +0700

    add authorize token

---
+++ backend/api-gateway/src/main/java/com/duolingo/clone/apigateway/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.duolingo.clone.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+
+}
--- backend/challenge-service/src/main/java/com/duolingo/clone/challengeservice/ChallengeServiceApplication.java
+++ backend/challenge-service/src/main/java/com/duolingo/clone/challengeservice/ChallengeServiceApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.duolingo.clone.challengeservice.client")
-@EnableRetry
 public class ChallengeServiceApplication {
 
 	public static void main(String[] args) {
